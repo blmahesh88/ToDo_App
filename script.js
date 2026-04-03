69a4f8314da5a4bf444ca29e23c461fdd2fdb73c
blmahesh88 <mahesh.kodaje@gmail.com>
2026-04-03 10:23:30 +0530

    Version 3 - added dark mode

--- script.js
+++ script.js
@@ -5,12 +5,27 @@ function addTask() {
     if (task === "") return;
 
     let li = document.createElement("li");
-    li.textContent = task;
 
-    li.onclick = function () {
-        this.remove();
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.onclick = function () {
+        li.style.textDecoration = this.checked ? "line-through" : "none";
     };
+
+    // delete button
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.onclick = function () {
+        li.remove();
+    };
+
+    li.appendChild(checkbox);
+    li.appendChild(document.createTextNode(" " + task + " "));
+    li.appendChild(deleteBtn);
 
     document.getElementById("taskList").appendChild(li);
     input.value = "";
+}
+function toggleDarkMode() {
+    document.body.classList.toggle("dark");
 }
